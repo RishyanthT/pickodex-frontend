0922c519a67fba36f98a52694878dab97e0f2831
Rishyanth Thotakura <rishyanthvarma555@gmail.com>
2026-03-28 03:26:34 -0400

    new with netlify test for mobile

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://picko-boot.onrender.com'
+  apiUrl: 'https://picko-boot.onrender.com',
+  brokerUrl: 'wss://picko-boot.onrender.com/ws-endpoint'
 };
